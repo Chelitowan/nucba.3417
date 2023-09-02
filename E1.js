339be9193434219f18dcc9edc110927de8a2e5b3
chelitowan <chelitowan@hotmail.com>
2023-09-02 19:12:20 -0300

    first commit

---
+++ E1.js
@@ -0,0 +1,14 @@
+// 1- Crear una función que reciba un número por parámetro e indique en consola si el número es par o impar.
+
+function numerosParOImpar(numero) {
+    if (numero % 2 === 0) {
+        console.log("El numero es par");
+    } else {
+        console.log("El numero es impar");
+    }
+}
+
+numerosParOImpar(33);
+numerosParOImpar(48);
+numerosParOImpar(67);
+numerosParOImpar(118);
